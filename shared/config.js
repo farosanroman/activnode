346bf6c1e35bd613bc76e730b152f949ab3b0893
farosanroman <ppazpurua@gmail.com>
2018-09-22 05:27:35 -0400

    ppa

--- shared/config.js
+++ shared/config.js
@@ -1,8 +1,8 @@
 const config = {
     user: 'admin',
-    password: 'pag9539961$',
-    server: '206.72.117.220', 
-    database: 'fuentes2015',
+    password: 'wwwwww$',
+    server: '20wwwww', 
+    database: 'wwwww',
     
 
 };
